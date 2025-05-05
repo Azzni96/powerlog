e9723f3c659b58036a88d90e28e21e4cdfb925e5
Azzni96 <nihadazzam96@gmail.com>
2025-05-06 02:10:10 +0300

    korjaus index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -45,11 +45,11 @@ app.use(errorHandler);
 app.get("/api/test", (req, res) => {
   res.json({ message: "API is working" });
 });
-if (process.env.NODE_ENV !== 'test') {
+
   const PORT = process.env.PORT || 3000;
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
   });
-}
+
 
 export default app;
